23a41d570677e40028d3844784ec4ed8081f07da
Alex McConnell <alexmcc.dev@gmail.com>
2021-04-21 12:01:03 -0400

    Deploy Next.js to out

--- src/components/Layout/PageWrapper.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-interface IPageWrapperProps {}
-
-const PageWrapper: React.FC<IPageWrapperProps> = ({ children }) => {
-    return <section className="flex h-full justify-center items-center px-4">{children}</section>
-}
-
-export default PageWrapper
